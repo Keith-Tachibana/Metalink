0d6e67cf0347d6632bfadc621159a42046999589
Andrew <awhsg11@gmail.com>
2020-03-18 23:12:10 +0000

    Added Header to Components and Added IMG For Header

--- client/components/home-page.jsx
+++ client/components/home-page.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import PostsList from './postsList';
+import Header from './header';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="h-50 text-white">
-                    navbar
-        </div>
+        <Header />
         <h5 className="col-12 d-flex justify-content-center  text-white">Good Evening, SonicMetal15</h5>
         <p className="col-12 d-flex justify-content-center font-weight-bold  text-white">Here are the latest posts...</p>
         <PostsList />
         <div className="row fixed-bottom">
-          <div className="col-4">Create Post</div>
-          <div className="col-4">Edit Post</div>
-          <div className="col-4">Exit</div>
+          <div className="col-4 text-white d-flex justify-content-center">Create Post</div>
+          <div className="col-4 text-white d-flex justify-content-center">Edit Post</div>
+          <div className="col-4 text-white d-flex justify-content-center">Exit</div>
         </div>
       </div>
     );
