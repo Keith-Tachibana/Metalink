155136f7d3ea03dcb93682c8bce25c556834260a
Jack Chou <jcacct@protonmail.ch>
2020-03-18 12:55:18 -0700

    Added login screen component, added import statment and render login screen component to app.jsx

---
+++ client/components/login-screen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class LoginPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <img src="./images/login.jpg" alt="login" />
+        <form action="">
+          <label htmlFor="login">Choose your destiny:</label>
+          <input type="option" />
+        </form>
+      </div>
+    );
+  }
+}
