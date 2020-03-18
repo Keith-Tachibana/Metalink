61e0830111b7d06fb10a3da2e83e9ed8dc7a3fdf
Jack Chou <jcacct@protonmail.ch>
2020-03-18 15:13:44 -0700

    Added styling for spacing throughout the page, added class login-headimg to style the header image

--- client/components/login-screen.jsx
+++ client/components/login-screen.jsx
@@ -18,22 +18,24 @@ export default class LoginPage extends React.Component {
   render() {
     return (
       <div className="text-center">
-        <img className="img-fluid" src="./images/login.jpg" alt="login" />
-        <h1>Welcome to Metalink</h1>
-        <h3>Your link to all things metal</h3>
-        <form className="dropdown" action="">
+        <img className="img-fluid login-headimg mb-3" src="./images/login.jpg" alt="login" />
+        <h1 className="mb-3">Welcome to Metalink</h1>
+        <h3 className="mb-5">Your link to all things metal</h3>
+        <form className="dropdown mb-5 pb-5" action="">
           <label htmlFor="login">Username</label>
-          <select name="login" id="login">
+          <select className="" name="login" id="login">
             {
               this.state.users.map(user => {
                 return (
-                  <option key={user.userId} value={user.screenName}>{user.screenName}</option>
+                  <option key={user.userId}
+                    value={user.screenName.replace(/\s/g, '').toLowerCase()}>
+                    {user.screenName}</option>
                 );
               })
             }
           </select>
         </form>
-        <a href="">Sign up for an account here</a>
+        <a className="mt-5 pt-5" href="">Sign up for an account here</a>
       </div>
     );
   }
