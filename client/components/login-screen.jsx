4e4aab840c5955d3af9274dc2989d67e0d1aa0b3
Jack Chou <jcacct@protonmail.ch>
2020-03-18 13:31:56 -0700

    Added render method with the initial setup for Login Page

--- client/components/login-screen.jsx
+++ client/components/login-screen.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 export default class LoginPage extends React.Component {
-
   render() {
     return (
-      <div>
-        <img src="./images/login.jpg" alt="login" />
-        <form action="">
-          <label htmlFor="login">Choose your destiny:</label>
-          <input type="option" />
+      <div className="text-center">
+        <img className="img-fluid" src="./images/login.jpg" alt="login" />
+        <h1>Welcome to Metalink</h1>
+        <h3>Your link to all things metal</h3>
+        <form className="dropdown" action="">
+          <label htmlFor="login">Username</label>
+          <select name="login" id="login">
+            <option value="stuff">Stuff</option>
+            <option value="stuff">Stuff</option>
+            <option value="stuff">Stuff</option>
+          </select>
         </form>
+        <a href="">Sign up for an account here</a>
       </div>
     );
   }
