55f2996a7b075ba09258447cf59bee62d7ed11bb
Jack Chou <jcacct@protonmail.ch>
2020-03-21 11:34:08 -0700

    Added input handler to wait for user to stop typing

--- client/components/search-concerts.jsx
+++ client/components/search-concerts.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 export default class SearchConcerts extends Component {
-  componentDidMount() {
-    this.getConcerts();
+  constructor(props) {
+    super(props);
+    this.state = {
+      concerts: []
+    };
+    this.timeout = null;
+    this.getConcerts = this.getConcerts.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    if (this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => this.getConcerts(value), 500);
   }
 
   getConcerts(postalCode) {
     fetch(`/api/concerts/${postalCode}`)
       .then(res => res.json())
-      .then(concerts => concerts)
+      .then(concerts => this.setState({ concerts }))
       .catch(err => console.error(err));
   }
 
@@ -18,8 +30,16 @@ export default class SearchConcerts extends Component {
         <h1 className="text-center mt-4">Concerts near you</h1>
         <form className="form-group-sm mb-5 pb-5 px-5">
           <label className="float-left" htmlFor="search">ZipCode</label>
-          <input type="text" className="form-control" placeholder="Enter Zipcode"/>
+          <input onChange={this.handleChange}type="text" className="form-control" placeholder="Enter Zipcode" />
         </form>
+        <table>
+
+        </table>
+        <ul>
+          {/* {
+            this.state.concerts.map()
+          } */}
+        </ul>
       </div>
     );
   }
