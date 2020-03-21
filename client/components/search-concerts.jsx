bc2867c1f5be9f9f7e5717b6b0841a423d317f65
Jack Chou <jcacct@protonmail.ch>
2020-03-21 10:06:43 -0700

    Created search concerts jsx file, added to app component

---
+++ client/components/search-concerts.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SearchConcerts extends Component {
+  render() {
+    return (
+      <h1 className="text-center mt-4">Concerts near you</h1>
+    );
+  }
+}
