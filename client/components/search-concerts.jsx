b452a52f2cc3bdd38898ab87e0f2bb3a6a2bf2bf
Jack Chou <jcacct@protonmail.ch>
2020-03-21 12:56:45 -0700

    Added conditional to display loading message if loading, styling to results table

--- client/components/search-concerts.jsx
+++ client/components/search-concerts.jsx
@@ -4,7 +4,9 @@ export default class SearchConcerts extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      concerts: []
+      concerts: [],
+      message: '',
+      loading: true
     };
     this.timeout = null;
     this.getConcerts = this.getConcerts.bind(this);
@@ -14,32 +16,49 @@ export default class SearchConcerts extends Component {
   handleChange(event) {
     const { value } = event.target;
     if (this.timeout) clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => this.getConcerts(value), 500);
+    this.timeout = setTimeout(() => this.getConcerts(value), 1000);
   }
 
   getConcerts(postalCode) {
-    fetch(`/api/concerts/${postalCode}`)
-      .then(res => res.json())
-      .then(concerts => this.setState({ concerts }))
-      .catch(err => console.error(err));
+    if (/^\d{5}(?:[-\s]\d{4})?$/g.test(postalCode)) {
+      fetch(`/api/concerts/${postalCode}`)
+        .then(res => res.json())
+        .then(concerts => this.setState({ concerts }))
+        .catch(err => console.error(err))
+        .finally(() => this.setState({ loading: false }));
+    } else {
+      return this.setState({ message: 'Valid zipcode please' });
+    }
   }
 
   render() {
+    const resultItems = this.state.concerts.map((concert, idx) => {
+      return (
+        <tbody key={idx} className="table-hover">
+          <tr>
+            <td>{concert.date}: {concert.venues}, {concert.location}</td>
+          </tr>
+          <tr>
+            <td>{concert.name}</td>
+          </tr>
+        </tbody>
+      );
+    });
+    // const messages = this.state.loading ? <h4>Loading...</h4> : 'stuff'
     return (
       <div>
         <h1 className="text-center mt-4">Concerts near you</h1>
-        <form className="form-group-sm mb-5 pb-5 px-5">
+        <form className="form-group-sm mb-2 px-5">
           <label className="float-left" htmlFor="search">ZipCode</label>
-          <input onChange={this.handleChange}type="text" className="form-control" placeholder="Enter Zipcode" />
+          <input onChange={this.handleChange}
+            type="text" className="form-control"
+            placeholder="Enter Zipcode" pattern="/^\d{5}(?:[-\s]\d{4})?$/g" />
         </form>
-        <table>
-
-        </table>
-        <ul>
-          {/* {
-            this.state.concerts.map()
-          } */}
-        </ul>
+        <div className="concertsearch-table table-responsive-sm px-4">
+          <table className="table table-sm table-dark table-striped">
+            {resultItems}
+          </table>
+        </div>
       </div>
     );
   }
