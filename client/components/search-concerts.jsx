1dd5f5139445521ecc19d04bbfef0dd6fca78aa6
Jack Chou <jcacct@protonmail.ch>
2020-03-21 10:23:50 -0700

    Edited error message in express endpoint for concert search for clarity, Added getConcerts method

--- client/components/search-concerts.jsx
+++ client/components/search-concerts.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 export default class SearchConcerts extends Component {
+  componentDidMount() {
+    this.getConcerts();
+  }
+
+  getConcerts(postalCode) {
+    fetch(`/api/concerts/${postalCode}`)
+      .then(res => res.json())
+      .then(concerts => concerts)
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div>
         <h1 className="text-center mt-4">Concerts near you</h1>
         <form className="form-group-sm mb-5 pb-5 px-5">
           <label className="float-left" htmlFor="search">ZipCode</label>
-          <input type="text"/>
+          <input type="text" className="form-control" placeholder="Enter Zipcode"/>
         </form>
       </div>
     );
