534695c9b47f3002b35e02b8b57cfbd494ec7a7d
Andrew <awhsg11@gmail.com>
2020-03-24 00:02:50 +0000

    Added Header

--- client/components/create-post.jsx
+++ client/components/create-post.jsx
@@ -5,7 +5,13 @@ class CreatePost extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="text-center">What would you like to post?</h1>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
