48720a099e37637cd4eebb06a60b8dc2a75ed3f7
Keith Tachibana <SonicMetal15@users.noreply.github.com>
2021-05-20 02:45:22 -0700

    Default page

---
+++ client/components/default-page.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DefaultPage extends Component {
+  render() {
+    return(
+      <React.Fragment>
+            
+      </React.Fragment>
+    );
+  }
+}
+
+export default DefaultPage;
