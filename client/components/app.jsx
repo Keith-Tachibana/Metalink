3e5bd160923788efb1a7eb1618ad52634d3d9921
Keith Tachibana <SonicMetal15@users.noreply.github.com>
2021-05-20 02:55:59 -0700

    Finished default page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -21,6 +21,7 @@ import ChatRoom from './chat-room';
 import AboutPage from './about-page';
 import EmailPassword from './email-password';
 import ResetPassword from './reset-password';
+import DefaultPage from './default-page';
 
 class App extends Component {
   constructor(props) {
@@ -296,31 +297,7 @@ class App extends Component {
                 <Route path="/" exact render={props =>
                   <LoginPage {...props} getProfile={this.getProfile} />
                 } />
-                <Route path="*" render={props =>
-                  <React.Fragment>
-                    <header className="container-fluid mb-4">
-                      <div className="row">
-                        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex justify-content-center">
-                          <picture>
-                            <source srcSet="/images/metalink.webp" type="image/webp" />
-                            <source srcSet="/images/metalink.png" type="image/png" />
-                            <img src="/images/metalink.png" alt="Metalink Logo" className="img-fluid mb-3" />
-                          </picture>
-                          <h1 className="text-center mt-4"><em>404:</em> Page Not Found</h1>
-                        </div>
-                      </div>
-                    </header>
-                    <footer>
-                      <div className="container-fluid justify-content-center d-flex">
-                        <Link to="/">
-                          <button className="btn btn-danger btn-sm">
-                            <i className="fas fa-arrow-circle-left"></i> Login
-                          </button>
-                        </Link>
-                      </div>
-                    </footer>
-                  </React.Fragment>
-                } />
+                <Route path="*" component={DefaultPage} />
               </Switch>
             </Router>
           </SocketProvider>
